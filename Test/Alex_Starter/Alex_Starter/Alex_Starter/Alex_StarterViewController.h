0889caaa53a82d7bb5e3d337f343806b1f7fd1a8
alex-wangyuan <wangyuan.wy@gmail.com>
2013-03-21 00:13:01 +0800

    Create a new project with nothing

---
+++ Test/Alex_Starter/Alex_Starter/Alex_Starter/Alex_StarterViewController.h
@@ -0,0 +1,15 @@
+//
+//  Alex_StarterViewController.h
+//  Alex_Starter
+//
+//  Created by user on 13-3-21.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Alex_StarterViewController : UIViewController {
+    
+}
+
+@end
---
+++ Test/Alex_Starter/Alex_Starter/Alex_StarterTests/Alex_StarterTests.h
@@ -0,0 +1,17 @@
+//
+//  Alex_StarterTests.h
+//  Alex_StarterTests
+//
+//  Created by user on 13-3-21.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Alex_StarterTests : SenTestCase {
+@private
+    
+}
+
+@end
