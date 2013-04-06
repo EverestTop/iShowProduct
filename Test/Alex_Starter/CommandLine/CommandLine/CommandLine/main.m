9ab553b760df1559f0047303ae67751e038a9d24
alex-wangyuan <wangyuan.wy@gmail.com>
2013-04-06 23:43:26 +0800

    Create a new CommandLine project

---
+++ Test/Alex_Starter/CommandLine/CommandLine/CommandLine/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CommandLine
+//
+//  Created by Alex on 13-4-6.
+//  Copyright (c) 2013年 EverestTop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
